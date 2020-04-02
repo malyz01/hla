7ee788be097d00552d1ddf73bace99860bee725e
Mathias Bast <mathiasbast123@gmail.com>
2020-04-03 11:53:50 +1300

    made random number functions and tests

--- tests/db.test.js
+++ tests/db.test.js
@@ -19,3 +19,28 @@ test('check if get card is working', () => {
       expect(typeof (card)).toBe('object')
     })
 })
+
+test('check if getRandomNumbers works', () => {
+  return db.getRandomNumbers(testDb)
+    .then(cards => {
+      expect(cards.length).toBe(3)
+    })
+})
+
+test('check if getRandomNumbers updates drawn', () => {
+  return db.getRandomNumbers(testDb)
+    .then(card => {
+      expect(card.id).toBe(5)
+      expect(card.drawn).toBe(true)
+    })
+})
+
+test('check if get randomnumber function returns a random number', () => {
+  const num = db.getRandomNumber()
+  
+  expect(true).toBeTruthy()
+})
+
+// test('check if reshuffle is working' , ()) = > {
+//   return
+// }
