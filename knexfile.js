86f16b218a2419412b78e41943160a993d38f636
unknown <mattuy001@gmail.com>
2020-04-04 19:12:46 +1300

    added db pg

--- knexfile.js
+++ knexfile.js
@@ -5,47 +5,43 @@ module.exports = {
     client: "sqlite3",
     useNullAsDefault: true,
     connection: {
-      filename: "./dev.sqlite3"
-    }
+      filename: "./dev.sqlite3",
+    },
   },
 
   test: {
-    client: 'sqlite3',
+    client: "sqlite3",
     useNullAsDefault: true,
     connection: {
-      filename: ':memory:'
-    }
+      filename: ":memory:",
+    },
   },
-  
+
   staging: {
     client: "postgresql",
     connection: {
       database: "my_db",
       user: "username",
-      password: "password"
+      password: "password",
     },
     pool: {
       min: 2,
-      max: 10
+      max: 10,
     },
     migrations: {
-      tableName: "knex_migrations"
-    }
+      tableName: "knex_migrations",
+    },
   },
 
   production: {
     client: "postgresql",
-    connection: {
-      database: "my_db",
-      user: "username",
-      password: "password"
-    },
+    connection: process.env.DATABASE_URL,
     pool: {
       min: 2,
-      max: 10
+      max: 10,
     },
     migrations: {
-      tableName: "knex_migrations"
-    }
-  }
+      tableName: "knex_migrations",
+    },
+  },
 };
